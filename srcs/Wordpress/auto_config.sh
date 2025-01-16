1c02471e791dbf84703b74502aa10734a52ee737
Denise Erjavec <derjavec@k1r2p8.42mulhouse.fr>
2025-01-16 14:41:28 +0100

    agregar datos de wordpress al env

--- srcs/Wordpress/auto_config.sh
+++ srcs/Wordpress/auto_config.sh
@@ -88,7 +88,7 @@ if ! wp core is-installed --allow-root --path='/var/www/wordpress'; then
     echo "Instalando WordPress..."
     wp core install --allow-root \
         --url="${WORDPRESS_URL}" \
-        --title="${WORDPRESS_TITLE}" \
+        --title="My not so incredible wordpress site" \
         --admin_user="${WORDPRESS_ADMIN_USER}" \
         --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
         --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
